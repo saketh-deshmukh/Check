c6f01e61e41a5cfb50a38f44d14ae5cddd2ed6ca
Saketh <User@LAPTOP-K8FAMQNA>
2022-01-25 21:23:40 -0500

    gb

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Check
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("gb World!");
         }
     }
 }
